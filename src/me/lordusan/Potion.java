992fc5b68cabcc33688efa0c3eaa453c3947b166
BuildTools <yankeesmonkey@gmail.com>
2016-03-12 13:16:46 -0500

    Replace redundant strings

--- src/me/lordusan/Potion.java
+++ src/me/lordusan/Potion.java
@@ -21,9 +21,10 @@ public class Potion extends JavaPlugin {
 		
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		String perk = ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")";
 		if (cmd.getName().equalsIgnoreCase("potions") && sender instanceof Player) {
 			Player player = (Player) sender;
-			player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + 
+			player.sendMessage(perk +  ChatColor.DARK_RED + 
 					"Nightvision (/nv), Fire Resistance (/fr), Jump (/jb), Heal (/heal), Speed (/fast)");
 			return true;
 		}
@@ -34,21 +35,21 @@ public class Potion extends JavaPlugin {
             {
             	if (args[0].equalsIgnoreCase("on"))
             	{
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Night Vision has been enabled");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Night Vision has been enabled");
             		player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 999999999, 2));
             	}
             	else if (args[0].equalsIgnoreCase("off"))
             	{
             		player.removePotionEffect(PotionEffectType.NIGHT_VISION);
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Night Vision has been removed");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Night Vision has been removed");
             	}
             }
             else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Night Vision has been enabled");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Night Vision has been enabled");
                 player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 999999999, 2));
             }
 			} else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
 			}
 			return true;
 
@@ -60,21 +61,21 @@ public class Potion extends JavaPlugin {
             {
             	if (args[0].equalsIgnoreCase("on"))
             	{
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Fire Resistance has been enabled");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Fire Resistance has been enabled");
             		player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 999999999, 2));
             	}
             	else if (args[0].equalsIgnoreCase("off"))
             	{
             		player.removePotionEffect(PotionEffectType.FIRE_RESISTANCE);
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Fire Resistance has been removed");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Fire Resistance has been removed");
             	}
             }
             else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Fire Resistance has been enabled");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Fire Resistance has been enabled");
                 player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE, 999999999, 2));
             }
 			} else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
 			}
 			return true;
 
@@ -86,32 +87,33 @@ public class Potion extends JavaPlugin {
             {
             	if (args[0].equalsIgnoreCase("on"))
             	{
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Jump has been enabled");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Jump has been enabled");
             		player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999999, 2));
             	}
             	else if (args[0].equalsIgnoreCase("off"))
             	{
             		player.removePotionEffect(PotionEffectType.JUMP);
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Jump has been removed");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Jump has been removed");
             	}
             }
             else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Jump has been enabled");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Jump has been enabled");
                 player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999999, 2));
             }
 			} else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
 			}
 			return true;
 
 		}
+
 		if (cmd.getName().equalsIgnoreCase("heal") && sender instanceof Player) {
 			Player player = (Player) sender;
 			if (player.hasPermission("potion.heal")) {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "You have been healed");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "You have been healed");
                 player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 2));
 			} else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
 			}
 			return true;
 
@@ -123,21 +125,21 @@ public class Potion extends JavaPlugin {
             {
             	if (args[0].equalsIgnoreCase("on"))
             	{
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Speed has been enabled");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Speed has been enabled");
             		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, 2));
             	}
             	else if (args[0].equalsIgnoreCase("off"))
             	{
             		player.removePotionEffect(PotionEffectType.SPEED);
-            		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Speed has been removed");
+            		player.sendMessage(perk +  ChatColor.DARK_RED + "Speed has been removed");
             	}
             }
             else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Speed has been enabled");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Speed has been enabled");
                 player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, 2));
             }
 			} else {
-        		player.sendMessage(ChatColor.GRAY + "(" + ChatColor.DARK_PURPLE + "Potion" + ChatColor.GRAY + ")" +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
+        		player.sendMessage(perk +  ChatColor.DARK_RED + "Upgrade your rank to get this perk!");
 			}
 			return true;
 		}
